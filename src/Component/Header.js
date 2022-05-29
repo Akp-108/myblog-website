129945857ae1cbb2a3331e96e49a4f6e526054c4
Akp-108 <akp1081@gmail.com>
2022-05-29 15:22:45 +0530

    Blog-Project

---
+++ src/Component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container__header'>
+                <p>The</p>
+                <h1>Siren</h1>
+    </div>
+  )
+}
+
+export default Header
