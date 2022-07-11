e22aecdc7ae4a9aedd202d2338a9ee07f0acb33c
Akp-108 <akp1081@gmail.com>
2022-07-11 20:16:55 +0530

    a

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink} from 'react-router-dom';
 
 const Card = (props) => {
-  const { imgUrl, title, description, articleid,fulldescription,keyID } = props;
+  const { imgUrl, title, description, articleid,fulldescription} = props;
   return (
     <div >
     <NavLink state={{ title: title, img: imgUrl, description: description, fulldescription:fulldescription, id: articleid }} to={`/article/${articleid}`}>
